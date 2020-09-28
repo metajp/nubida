6be14678bd21847b7b762c896c5c50c905d6210f
정종필 <psyvlf@gmail.com>
2020-09-28 17:27:17 +0900

    404 오류페이지 등록

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask , render_template
 
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -16,6 +16,10 @@ naming_convention = {
 }
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
+
+
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 def create_app():
@@ -41,6 +45,8 @@ def create_app():
     app.register_blueprint(comment_views.bp)
     app.register_blueprint(vote_views.bp)
 
+    # 오류페이지
+    app.register_error_handler(404, page_not_found)
 
     # 필터
     from .filter import format_datetime
